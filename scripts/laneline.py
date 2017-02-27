c4d3a1c3007a1ec8c8f20e4e9667171c9e59e982
Nanda Raghunathan <nanda@thenandlabs.com>
2017-02-27 15:23:42 -0800

    Forgot files

--- scripts/laneline.py
+++ scripts/laneline.py
@@ -44,24 +44,24 @@ class LaneLine():
         out_img[self.nonzeroy[self.right_lane_inds], nonzerox[self.right_lane_inds]] = [0, 0, 255]
         return out_img, left_fitx, right_fitx
 
-    def visualize_band(self, binary_warped):
+    def visualize_band(self, undistorted, perspective):
         """
         Visualize the predicted lane lines with margin, on binary warped image
         save_file is a string representing where to save the image (if None, then just display)
         """
 
         # Create an image to draw on and an image to show the selection window
-        out_img = (np.dstack((binary_warped, binary_warped, binary_warped))*255).astype('uint8')
-        window_img = np.zeros_like(out_img)
+        out_img = np.zeros_like(undistorted)
+        window_img = np.zeros_like(undistorted)
 
         # Color in left and right line pixels
         out_img[self.nonzeroy[self.left_lane_inds], self.nonzerox[self.left_lane_inds]] = [255, 0, 0]
         out_img[self.nonzeroy[self.right_lane_inds], self.nonzerox[self.right_lane_inds]] = [0, 0, 255]
 
         # Generate x and y values for plotting
-        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+        ploty = np.linspace(0, undistorted.shape[0]-1, undistorted.shape[0])
+        left_fitx = self.left_fit[0]*ploty**2 + self.left_fit[1]*ploty + self.left_fit[2]
+        right_fitx = self.right_fit[0]*ploty**2 + self.right_fit[1]*ploty + self.right_fit[2]
 
         # Generate a polygon to illustrate the search window area
         # And recast the x and y points into usable format for cv2.fillPoly()
@@ -76,8 +76,13 @@ class LaneLine():
         # Draw the lane onto the warped blank image
         cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
         cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
-        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-        
+        lane_band = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+       
+        # Warp the blank back to original image space using inverse perspective matrix (Minv)
+        newwarp = perspective.unwarp(lane_band)
+
+        # Combine the result with the original image
+        result = cv2.addWeighted(undistorted, 1, newwarp, 0.6, 0)
         return result
 
     def vehicle_offset(self, image):
